397db88a0ba49fc84529f1a4edff747d4fc51eb5
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2026-02-14 19:18:58 +0500

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
-import { RiHeartFill } from "@remixicon/react";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
